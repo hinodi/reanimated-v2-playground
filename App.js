e60d8674e327afb574cbc2ddf9442dece6c3dee1
hinodi <huynhngocdinh98@gmail.com>
2021-04-15 11:57:24 +0700

    chore: add react-native-reanimated v2

--- App.js
+++ App.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Button, Dimensions} from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from 'react-native-reanimated';
 
 const App = () => {
-  return <View style={styles.container}></View>;
+  const offset = useSharedValue(0);
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{translateX: offset.value * DEVICES_WIDTH}],
+    };
+  });
+
+  const _onPress = () => (offset.value = withSpring(Math.random()));
+
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.box, animatedStyles]} />
+      <Button onPress={_onPress} title="Move" />
+    </View>
+  );
 };
 
 export default App;
+
+const {width: DEVICES_WIDTH} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+  },
+  box: {
+    width: 100,
+    height: 100,
+    borderRadius: 20,
+    backgroundColor: '#0000ff90',
   },
 });
